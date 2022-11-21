aac1e9e02b6e7e67724e0f86e8f77632cb4111c4
Camila Santos Santana  Costa <107818807+DevCamila-dev@users.noreply.github.com>
2022-11-20 23:51:24 -0300

    first commit

---
+++ Backend/Migrations/20221120002340_firstMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Viajar.Migrations
+{
+    public partial class firstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "destinos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome_destino = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    estado_destino = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    pais_destino = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    dataIda_destino = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    dataVolta_aluno = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Valor_destino = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_destinos", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "destinos");
+        }
+    }
+}
---
+++ Backend/Model/Destino.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Viajar.Model
+{
+
+
+
+    [Table("destinos")]
+    public class Destino
+    {
+
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("nome_destino")]
+        public string Nome { get; set; } 
+
+        [Column("estado_destino")]
+        public string Estado { get; set; }
+
+        [Column("pais_destino")]
+        public string Pais { get; set; }  
+
+        [Column("dataIda_destino")]
+        public string dataIda { get; set; } 
+
+        
+        [Column("dataVolta_aluno")]
+        public string dataVolta { get; set; } 
+
+        [Column("Valor_destino")]
+        public string Valor { get; set; } 
+
+
+
+
+
+    }
+}
